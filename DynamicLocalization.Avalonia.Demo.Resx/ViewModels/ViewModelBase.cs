14ff1bd2eb8d6f7167080ae09fe7939d5747421b
sakuya <xiao.yejun666@gmail.com>
2026-03-27 21:36:58 +0800

    优化项目命名空间，nuget package id似乎被占用了

---
+++ DynamicLocalization.Avalonia.Demo.Resx/ViewModels/ViewModelBase.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace DynamicLocalization.Avalonia.Demo.Resx.ViewModels;
+
+/// <summary>
+/// Base class for all view models in the application.
+/// </summary>
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ DynamicLocalization.Avalonia.Demo.Resx/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace DynamicLocalization.Avalonia.Demo.Resx.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
